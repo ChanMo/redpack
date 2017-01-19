10b3f3c36a7a29dee025a106d2578ca5cb72aa11
ChanMo <chen.orange@aliyun.com>
2017-01-19 14:06:54 +0800

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,29 @@
 $(function(){
+
   /** prevent touch event **/
   $("body").on("touchmove", function(event){
     event.preventDefault();
+  });
+
+  $("[data-modal-close]").on("click", function(e){
+    e.preventDefault();
+    $(".modal").removeClass("modal-active");
+    $(".modal-bg").hide();
+  });
+
+  $("#main-action").on("click", function(event){
+    event.preventDefault();
+    $(".modal").addClass("modal-active");
+    $(".modal-bg").show();
+    $.ajax({
+      url: 'js/get_result.json',
+      dataType: 'json',
+      success: function(result){
+        console.log(result);
+        $(".modal p").html(result['message']);
+        $(".modal img").attr("src", result['image']);
+      }
+    });
   });
 
   /** autuscroll **/
